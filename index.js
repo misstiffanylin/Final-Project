dfb5e3e9afced2dd9ac1cb8446c41fbaaaf5d912
Tiffany Lin <tlin625@gmail.com>
2016-11-11 21:45:19 -0800

    active buttons clean up

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-    $('#emailForm').submit(function(submitEvent){
-        submitEvent.preventDefault();
-    })
-
-var genre = ['Chill House', 'Deep House', 'Trap/Hip Hop', 'Progressive House'];
-genre.forEach(function(value, key){
-    $('#playlist').append('<button class="btn btn-default" value=' + key + '>' + value +'</button> ');
-});
 
 
 $(document).ready(
     function(){
+
+        $('#emailForm').submit(function(submitEvent){
+            submitEvent.preventDefault();
+         })
+
+        var genre = ['Chill House', 'Deep House', 'Trap/Hip Hop', 'Progressive House'];
+            genre.forEach(function(value, key){
+            $('#playlist').append('<button class="btn btn-default" value=' + key + '>' + value +'</button> ');
+        });
+
+        $("#playlist .btn").click ( function() {
+            var button = $(this);
+            $("#playlist .btn").removeClass("black");
+            button.addClass("black");
+        });
+
+        $("ul.nav li a").click( function() {
+            var button = $(this);
+            $("ul.nav li a").removeClass("black");
+            button.addClass("black");
+        });
+
         $("#playlist button:nth-child(2)").click(function(){
             $(".list").addClass('hiddenPlaylist');
             $("#chill").removeClass('hiddenPlaylist');
@@ -34,6 +48,8 @@ $(document).ready(
             $(".list").addClass('hiddenPlaylist');
             $("#progressive").removeClass('hiddenPlaylist')
         });
+
+        //
     }
 );
 
